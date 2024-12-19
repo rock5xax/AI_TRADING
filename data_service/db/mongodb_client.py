a8dd2a71d35e14b61dc60f9ad000c11b2d915f63
rock5xax <choudhary.kuldeep844@gmail.com>
2024-12-19 18:00:00 +0000

    update_01

--- data_service/db/mongodb_client.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class MongoDBClient:
-    def __init__(self, uri, database_name):
-        self.client = MongoClient(uri)
-        self.db = self.client[database_name]
-
-    def get_collection(self, collection_name):
-        return self.db[collection_name]
--- data_service/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from flask import Flask
-
-
-@pytest.fixture
-def flask_app():
-    """Fixture to create a Flask app for testing."""
-    app = Flask(__name__)
-    yield app
